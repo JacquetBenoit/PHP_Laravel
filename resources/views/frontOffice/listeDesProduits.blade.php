2727b1808a845896f61552ad297e2173aa84a0c3
Benoit Jacquet <benoit.jacquet@Digital-Grenoble.local>
2019-03-01 09:59:55 +0100

    ajusstement css

--- resources/views/frontOffice/listeDesProduits.blade.php
+++ resources/views/frontOffice/listeDesProduits.blade.php
@@ -6,11 +6,17 @@
 @section('content')
 
 @foreach ($articles as $art)
-   <div class="flex-center">   
-      <img src="{{$art['image']}}" alt="carotte" class="imgsize">
-      {{$art['nom']}}
-      {{$art['prix']}} €
-   </div>
+   <div class="cont_elements">
+      <form action="panier.php" method="post" id="form1">
+         <div class="element">   
+            <img src="{{$art['image']}}" alt="carotte" class="imgsize">
+            {{$art['nom']}}
+            {{$art['prix']}}€
+            <input type ="checkbox" class="check" name="check[]" id="recep" value="#"/>
+         </div>
+      </form>   
+   </div>   
 @endforeach
+<button type="submit" form="form1" value="submit" class="btn btn-secondary btn-sm">Submit</button>
 
 @endsection
