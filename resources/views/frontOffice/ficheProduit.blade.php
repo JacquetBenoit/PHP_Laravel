54ca7fc5daa8357b0e13cb6172238514153769e0
Chloe <chloe.marquet.1991@gmail.com>
2019-03-05 14:18:41 +0100

    Passage du prix en centimes en euros et ajout de la descrption dans la fiche produit

--- resources/views/frontOffice/ficheProduit.blade.php
+++ resources/views/frontOffice/ficheProduit.blade.php
@@ -16,7 +16,8 @@
         <div class="container-right">
            <!-- Affiche le nom et le prix de l'article -->
             <h2>{{$produit -> nom ."\n"}}</h2>
-            <p>{{$produit -> prix . " €"}}</p>
+            <p>{{$produit -> prix /100}} €</p>
+
            
            <!-- Quantité -->
            <input type="number" name="quantity[{{$produit->nom}}]" value="1" min="0" max="10" required/>
@@ -24,6 +25,9 @@
            <!-- Case à cocher -->
            <label for="case">Ajouter au panier</label>
            <input type="checkbox" name="add_to_basket[]" value="">
+
+            <!-- Affiche la description de l'article -->
+            <p>{{$produit->description}}</p>
         </div>
     </div>
 </div>
