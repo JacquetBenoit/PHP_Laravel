7e1c4ff2b08617446a9a512d9ce9912661020a13
Chloe <chloe.marquet.1991@gmail.com>
2019-03-07 17:48:25 +0100

    Ajout du bouton modifier et supprimer d'un produit

--- resources/views/admin/gestionProduits.blade.php
+++ resources/views/admin/gestionProduits.blade.php
@@ -8,31 +8,34 @@
 
     <a href="{{route('gestionProduitsAjouter')}}" type="button" class="btn btn-light">Ajouter un produit</a>
 
-        <table class="table table-hover table-hover table-striped table-dark table-responsive">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nom du produit</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Poids</th>
-                    <th scope="col">Prix</th>
-                    <th scope="col">Stock</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($produits as $produit)
-                <tr>
-                    <th scope="row">{{$produit->id}}</th>
-                    <td>{{$produit->nom}}</td>
-                    <td> <img  class="img-fluid" src="{{asset($produit->image)}}" alt = "Stan Smith"></td>
-                    <td>{{$produit->description}}</td>
-                    <td>{{$produit->poids}}</td>
-                    <td>{{$produit -> prix /100}} €</td>
-                    <td>{{$produit->stock}}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+    {{-- TABLEAU DES PRODUITS --}}
+    <table class="table table-hover table-hover table-striped table-dark table-responsive">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom du produit</th>
+                <th scope="col">Image</th>
+                <th scope="col">Description</th>
+                <th scope="col">Poids</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($produits as $produit)
+            <tr>
+                <th scope="row">{{$produit->id}}</th>
+                <td>{{$produit->nom}}</td>
+                <td> <img  class="img-fluid" src="{{asset($produit->image)}}" alt = "Stan Smith"></td>
+                <td>{{$produit->description}}</td>
+                <td>{{$produit->poids}}</td>
+                <td>{{$produit -> prix /100 ."€"}}</td>
+                <td>{{$produit->stock}}</td>
+                <td><a href="{{route('gestionProduitsModifier', $produit->id)}}" type="button" class="btn btn-light">Modifier</a></td>
+                <td><a href="{{route('gestionProduitsDelete', $produit->id)}}" type="button" class="btn btn-light">Supprimer</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
