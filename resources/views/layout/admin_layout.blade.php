642e8b0349b77e5e925fc90140cc8cffbca86f91
Chloe <chloe.marquet.1991@gmail.com>
2019-03-07 17:59:56 +0100

    Pas de modification visible, peu être un espace

--- resources/views/layout/admin_layout.blade.php
+++ resources/views/layout/admin_layout.blade.php
@@ -71,11 +71,7 @@
             </nav>
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div class="chartjs-size-monitor marginTable" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
                 <h2 class="titleSection">@yield('titleSection')</h2>
-                <div class="table-responsive">
-                    {{--<table class="table table-sm">--}}
                         @yield('content')
-                    {{--</table>--}}
-                </div>
             </main>
         </div>
     </div>
