1aa11657772acfe57bc32b075866887f8269c6c2
Chloe <chloe.marquet.1991@gmail.com>
2019-03-07 13:36:28 +0100

    Ajout de  et

--- app/Produit.php
+++ app/Produit.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     protected $table = 'produit';
+    public $timestamps = false;
+    protected $fillable = ['nom', 'description', 'prix', 'poids', 'stock', 'id_categorie', 'image'];
 }
