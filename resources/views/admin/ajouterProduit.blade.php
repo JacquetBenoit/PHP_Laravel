2c8d60c1d175a7dc1f6ff5f209bf8c742fa2ea32
Chloe <chloe.marquet.1991@gmail.com>
2019-03-07 13:38:57 +0100

    Mise en place du formulaire

--- resources/views/admin/ajouterProduit.blade.php
+++ resources/views/admin/ajouterProduit.blade.php
@@ -7,16 +7,43 @@
 @section('titleSection', 'Ajouter un produit')
 
 
-<form>
+<form method="post" action="/gestions-produits/">
+    @csrf
     <div class="form-group">
-        <label for="nom">{{$produit->nom}}</label>
-        <input type="text" class="form-control" id="nom" placeholder="Nom du produit">
+        <label for="nom">Nom du produit</label>
+        <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom du produit">
     </div>
     <div class="form-group">
-        <label for="description">{{$produit->description}}</label>
-        <textarea class="form-control" id="description" rows="3" placeholder="Description"></textarea>
+        <label for="description">Description</label>
+        <textarea class="form-control" id="description" rows="3" name="description" placeholder="Description"></textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Ajouter</button>
+    <div class="form-group">
+        <label for="prix">Prix</label>
+        <input type="text" class="form-control" id="prix" name="prix" placeholder="Prix">
+    </div>
+    <div class="form-group">
+        <label for="poids">Poids</label>
+        <input type="text" class="form-control" id="poids" name="poids" placeholder="Poids">
+    </div>
+    <div class="form-group">
+        <label for="stock">Stock</label>
+        <input type="text" class="form-control" id="stock" name="stock" placeholder="Stock">
+    </div>
+    <div class="form-group">
+        <label for="categorie">Categorie</label>
+        <select class="form-control" id="categorie" name="id_categorie">
+            <option value="1">1 Snowboard</option>
+            <option value="2">2 Lifestyle</option>
+            <option value="3">3 Running</option>
+            <option value="4">4 Basketball</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="stock">Photo du produit</label>
+        <input type="text" class="form-control" id="photo" name="image" placeholder="/images/nomDuFichier.jpg">
+    </div>
+
+    <button type="submit" class="btn btn-secondary">Ajouter</button>
 </form>
 
 @endsection
