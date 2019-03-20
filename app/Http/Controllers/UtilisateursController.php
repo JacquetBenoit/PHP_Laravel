f6d8417f1f59dbc6c364b6b5ba24ff15987f1587
Chloe <chloe.marquet.1991@gmail.com>
2019-03-20 15:35:34 +0100

    Méthodes ajouterPanier et panier : étape 1 passage des données en durs

--- app/Http/Controllers/UtilisateursController.php
+++ app/Http/Controllers/UtilisateursController.php
@@ -2,13 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Produit;
 use Illuminate\Http\Request;
 
 class UtilisateursController extends Controller
 {
-    public function panier()
+    public function ajouterPanier(Request $request)
     {
-        return view('frontOffice/panier');
+        $request->input('add_to_basket');
+        dd($request->input('add_to_basket'));
+        /*
+        dd('ajoute panier ', $request->input('add_to_basket'));
+        $request->session()->put('produits', $request->input('add_to_basket'));*/
+
+        return redirect(route('panier'));
+    }
+
+    public function panier(Request $request)
+    {
+        $panier =
+            [
+                [
+                    "id" => 2,
+                    "quantite" => 2,
+                ],
+                [
+                    "id" => 14,
+                    "quantite" => 1,
+                ],
+            ];
+
+        $panierProduits = [];
+        foreach ($panier as $produit)
+        {
+            $p = Produit::find($produit["id"]);
+
+            array_push($panierProduits, [
+                'produit' => $p, // recupérer produit dans BDD
+                'quantite' => $produit["quantite"], // Recupérer quantité
+            ]);
+        }
+        /*$idProduits = $request->session()->get('produits');
+
+        $produits = [];
+        foreach ($idProduits as $idProduit)
+        {
+            $p = Produit::find($idProduit);
+            array_push($produits, $p);
+            dump($p);
+        }*/
+       //dd($produits);
+//        return view('frontOffice/panier', ['produit'=>$produits]);
+        return view('frontOffice/panier', ['panierProduits'=>$panierProduits]);
     }
 
     public function coordonnees()
