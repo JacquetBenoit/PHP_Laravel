f335d50ac0a98946559ff7ffb70a77fffa630a27
JacquetBenoit <bendigit20@gmail.com>
2019-03-25 16:49:29 +0100

    mise en forme du formulaire mon compte

--- resources/views/frontOffice/monCompte.blade.php
+++ resources/views/frontOffice/monCompte.blade.php
@@ -4,21 +4,49 @@
 
 
 @section('content')
-    @dump ($user)
 
+<div class="container">
 <form method="POST" action="{{route('editCompte')}}">
     @csrf
-    <input type="text" value="{{$user->name}}" name="userName">
-    <input type="email" value="{{$user->email}}" name="userMail">
-    <input type="text" value="{{$user->customer->NAME}}" name="userCustName">
-    <input type="text" value="{{$user->customer->FIRSTNAME}}" name="userFirstname">
-    <input type="text" value="{{$customer->address->STREET1}}" name="street1">
-    <input type="text" value="{{$customer->address->STREET2}}" name="street2">
-    <input type="number" value="{{$customer->address->POSTCODE}}" name="postcode">
-    <input type="text" value="{{$customer->address->TOWN}}" name="town">
-    <input type="text" value="{{$customer->address->COUNTRY}}" name="country">
+    <div class="form-group">
+        <label for="descrition">Pseudo</label>
+        <input type="text" value="{{$user->name}}" name="userName" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Mail</label>
+        <input type="email" value="{{$user->email}}" name="userMail" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Nom</label>
+        <input type="text" value="{{$user->customer->NAME}}" name="userCustName" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Prenom</label>
+        <input type="text" value="{{$user->customer->FIRSTNAME}}" name="userFirstname" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Adresse 1</label>
+        <input type="text" value="{{$customer->address->STREET1}}" name="street1" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Adresse 2</label>
+        <input type="text" value="{{$customer->address->STREET2}}" name="street2" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Code postal</label>
+        <input type="text" value="{{$customer->address->POSTCODE}}" name="postcode" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Ville</label>
+        <input type="text" value="{{$customer->address->TOWN}}" name="town" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="descrition">Pays</label>
+        <input type="text" value="{{$customer->address->COUNTRY}}" name="country" class="form-control">
+    </div>
     <button type="submit" class="btn btn-light">Submit</button>
 </form>
+</div>
 
 
 
