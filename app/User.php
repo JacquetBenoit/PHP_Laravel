74c962ca7264a9c4bedfed8aee42c216e3d354f0
vivienkoelsch88 <vivien.koelsch@le-campus-numerique.fr>
2019-03-25 11:27:06 +0100

    Création de la migration user/customer

--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function customer()
+    {
+        return $this->hasone('App\Customer', 'id_USER', 'id');
+    }
 }
