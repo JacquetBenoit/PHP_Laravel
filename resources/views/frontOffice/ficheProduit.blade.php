2e0eb50105f7caa83970e237a698dfd78068e6da
vivienkoelsch88 <vivien.koelsch@le-campus-numerique.fr>
2019-03-29 10:29:57 +0100

    Les fiches produits affichent les promotions

--- resources/views/frontOffice/ficheProduit.blade.php
+++ resources/views/frontOffice/ficheProduit.blade.php
@@ -15,13 +15,34 @@
             <div class="img">        		
             <img src="{{$product->IMAGE}}" alt="vegetables-images" class="image">
             </div>
+            @if($product->id_PROMOTION != "" && $product->id_PROMOTION != 0)
+                @if ($product->Promotion->VALUE != "")
+                    {{"Réduction : -" . $product->Promotion->VALUE . "€ sur le produit"}}
+                @endif
+                @if ($product->Promotion->PERCENT != "")
+                    {{"Réduction : -" . $product->Promotion->PERCENT . "% sur le produit"}}
+                @endif
+                <br>
+            @endif
             {{$product->DESCRIPTION}}
         </div>
         
         <div class="container-right">
            <!-- Affiche le nom et le prix de l'article -->
             <h2>{{$product->NAME}}</h2>
-            <p>{{$product->PRICE/100}}€</p>
+            @if($product->id_PROMOTION != ""&& $product->id_PROMOTION != 0)
+                @if ($product->Promotion->VALUE != "")
+                    <br>
+                    {{$product->PRICE/100-$product->Promotion->VALUE}}€
+                @endif
+                @if ($product->Promotion->PERCENT != "")
+                    <br>
+                    @php $reduct = $product->PRICE/100 * $product->Promotion->PERCENT/100 @endphp
+                    {{$product->PRICE/100-$reduct}}€
+                @endif
+            @else
+                <p>{{$product->PRICE/100}}€</p>
+        @endif
            
            <!-- Quantité -->
            <input type="number" name="quantity" value="1" min="0" max="{{$product->STOCK}}" required/>
